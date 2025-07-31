a8f0639f880a37f3f095b4ad6fd51f890d548322
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2025-08-01 01:25:40 +0530

    db issues fixed

--- Backend/model/db_connect.py
+++ Backend/model/db_connect.py
@@ -1,8 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-MONGO_URL = "mongodb://localhost:27017" 
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["smart_learning"]
+db = client["CourseGen"]
 
 print("db connected")
